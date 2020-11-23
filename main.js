7afb30645f53b9dc4f4075dbbb8bb63103d719b4
Stanislao <lombardo.stanislao94@xn--lieb-spa>
2020-11-23 14:55:33 +0100

    Inizio

---
+++ main.js
@@ -0,0 +1,18 @@
+// fare una chiamata ajax per recuperare una decina di dischi musicali e utilizzando vue, stampare a schermo una card per ogni album.
+// L'API da chiamare è: https://flynn.boolean.careers/exercises/api/array/music
+
+
+
+var app = new Vue({
+    el: '#root',
+
+        data: {
+
+        },
+
+        methods: {
+
+        },
+
+        mounted:
+});
